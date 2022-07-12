53f67de282b9a70d47b3bb4d6e7cff71fd0c5ffb
albert <albert>
2022-07-12 10:37:41 +0200

    pff

--- config/Db.go
+++ config/Db.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	db, err := gorm.Open(postgres.Open("postgres://awotxvic:uxJrwNzW7MYyb9BaJUThmdVAIP0fJ4jr@surus.db.elephantsql.com/awotxvic"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("postgres://ukzhonijtpkwdc:b289a4d18e72984552bd10f251ed0e3a869a5bb9c9c3dcd4bc641f2bfd5e7428@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d2qmn2h3i98o9t"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
